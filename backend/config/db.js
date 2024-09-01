85437b7d09140f0c8502bf91b6852f720e000f6d
naveen <naranaveen576@gmail.com>
2024-09-01 15:05:13 +0530

    added controllers

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://naranaveen576:Naveen576@faculty.2z27b.mongodb.net/?retryWrites=true&w=majority&appName=faculty', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+    await mongoose.connect(
+      "mongodb+srv://naranaveen:Naveen576@fms.2z27b.mongodb.net/?retryWrites=true&w=majority&appName=FMS"
+    );
+    console.log("Connected");
   } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+    console.log(error);
   }
 };
 
